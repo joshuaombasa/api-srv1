fe4f956b95d659b631a2f53834eb6c7f4e999944
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-04 21:38:34 +0300

    initial commit

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,17 @@
+import { CustomError } from './custom-error';
+
+class DatabaseConnectionError extends CustomError {
+  statusCode = 400;
+  reason = 'Error connecting to database';
+  constructor() {
+    super('Error connecting to database');
+
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
+  }
+}
+
+export { DatabaseConnectionError };
