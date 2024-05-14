f073b63836c23868ed8ceedbd5a29415cadd2078
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-14 22:08:16 +0300

    add integration tests

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,38 +1,7 @@
-import express from 'express';
-import { NotFoundError } from './errors/not-found-error';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
+import { app } from './app';
 
 const PORT = 3000;
-
-import { signupRouter } from './routes/signup';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { currentUserRouter } from './routes/current-user';
-
-import { errorHandler } from './middleware/error-handler';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(express.json());
-
-app.use(
-  cookieSession({
-    signed: false,
-    secure: false,
-  })
-);
-
-app.use(signupRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(currentUserRouter);
-
-app.all('*', () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
 
 const start = () => {
   mongoose
